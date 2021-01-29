50a53f2c90e05cfb14886e2a5a55adb6b6ffffd0
baothi <baothi246@gmail.com>
2021-01-29 09:47:10 +0700

    add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dir[File.dirname(__FILE__) + '/seeds/*.rb'].sort.each do |file|
+  puts "Seeds #{file} ..."
+  require file
+end
